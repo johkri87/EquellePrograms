697f02718bcabf875ba09ce08507135e5d038a78
johkri87 <johkri87@gmail.com>
2018-02-05 21:53:03 +0100

    add parameter data files and changed compile_scipt to take backend as parameter. (cuda, cpu etc)

--- compile_script.sh
+++ compile_script.sh
@@ -6,8 +6,8 @@ do
     bn=$(basename $i .equelle)
     nondim=""
     if [[ $nondim =~ (^| )$bn($| ) ]]; then
-        ../build-cuda8/compiler/ec -i $i --nondimensional --backend=cuda > "CompiledSrc/out_$bn.cpp"
+        ../build-cuda8/compiler/ec -i $i --nondimensional --backend=$1 > "CompiledSrc/out_$bn.cpp"
     else
-        ../build-cuda8/compiler/ec -i $i --backend=cuda > "CompiledSrc/out_$bn.cpp"
+        ../build-cuda8/compiler/ec -i $i --backend=$1 > "CompiledSrc/out_$bn.cpp"
     fi
 done
